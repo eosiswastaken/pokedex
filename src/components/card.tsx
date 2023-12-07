555023e82121928e2ec256b248ad5143e7225e8d
TheLordGibril <gabidule03@gmail.com>
2023-12-07 11:45:06 +0100

    card text color set to white

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Card() {
     return (
-        <div className="mx-auto flex min-h-screen max-w-screen-sm items-center justify-center">
+        <div className="mx-auto flex min-h-screen max-w-screen-sm items-center justify-center text-white">
             <div className="w-[128px] h-[180px] rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1">
                 <div className="text-xs h-full w-full flex flex-col items-center justify-between bg-gray-800 back">
                     <div className="w-full h-[16px] grid grid-cols-card-title justify-between"><span>#175</span><span>Pikachu</span><span>O</span></div>
