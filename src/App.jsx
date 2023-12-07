c2fbea7bb1b7070e481caa335570dc151efea50f
TheLordGibril <gabidule03@gmail.com>
2023-12-07 13:28:52 +0100

    gl

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
 import Card from './components/card'
 import Header from './components/Header';
+import useAPI from './context/Context';
 
 function App() {
+  useAPI()
   return (
     <div className="App">
       <Header></Header>
