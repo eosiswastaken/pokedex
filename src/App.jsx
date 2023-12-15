6eca6c2e32526cc0818ada9ca6a36dce0752c0c0
TheLordGibril <gabidule03@gmail.com>
2023-12-15 13:03:43 +0100

    API fetched, data can now be displayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css';
 import Card from './components/card'
 import Header from './components/Header';
-import useAPI from './context/Context';
+import { APIContextProvider } from './context/Context';
 
 function App() {
-  useAPI()
   return (
-    <div className="App">
-      <Header></Header>
-      <Card></Card>
-    </div>
+    <APIContextProvider>
+      <div className="App">
+        <Header></Header>
+        <Card></Card>
+      </div>
+    </APIContextProvider>
   );
 }
 
