c0015ccd02b2e5562d20870138599b27775943fa
TheLordGibril <gabidule03@gmail.com>
2023-12-15 13:37:57 +0100

    merging

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { useAPI } from "../context/Context";
+import useAPI from "../context/Context";
 
 export default function Card() {
     const { pokemons } = useAPI();
     return (
-        <div className="flex flex-wrap">
+        <div className="flex flex-wrap gap-2 justify-center">
             {pokemons.map(u => (
                 <div key={u.id} className="items-center justify-center text-white">
-                    < div className="w-[128px] h-[180px] rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1" >
+                    < div className="w-[300px] h-[420px] rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1" >
                         <div className="text-xs h-full w-full flex flex-col items-center justify-between bg-gray-800 back">
                             <div className="w-full h-[16px] grid grid-cols-card-title justify-between"><span>#175</span><span>{u.name.fr}</span><span>O</span></div>
                             <div className="w-full flex justify-center"><img className="h-[72px]" src="téléchargement.jpg" alt="" /></div>
