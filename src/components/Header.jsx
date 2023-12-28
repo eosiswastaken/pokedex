b4ceebc895fc9b9af37b704e4d61d1509a11d8c8
Antoine Cerdan <antoinecerdan31@gmail.com>
2023-12-28 17:32:13 +0100

    replacing filters with select elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,19 @@
-import Filter from "./Filter"
 import "./Header.css"
 
 export default function Header(){
 
-    let generations = [
-        { name: 'All generations' },
-        { name: 'Gen. 1' },
-        { name: 'Gen. 2' },
-        { name: 'Gen. 3' },
-        { name: 'Gen. 4' },
-        { name: 'Gen. 5' },
-        { name: 'Gen. 6' },
-        { name: 'Gen. 7' },
-        { name: 'Gen. 8' },
-        { name: 'Gen. 9' },
-        { name: 'Gen. 10' }
-      ]
+    let generations = ['Gen. 1','Gen. 2','Gen. 3','Gen. 4','Gen. 5','Gen. 6','Gen. 7','Gen. 8','Gen. 9','Gen. 10',]
+    let types = ['Bug','Dark' ,'Dragon' ,'Electric' ,'Fairy' ,'Fighting' ,'Fire' ,'Flying' ,'Ghost' ,'Grass' ,'Ground' ,'Ice' ,'Normal' ,'Poison' ,'Psychic' ,'Rock' ,'Steel' ,'Water' ]
 
-      let types = [
-        { name: 'Type 1' },
-        { name: 'Type 2' },
-        { name: 'Type 3' },
-        { name: 'Type 4' },
-        { name: 'Type 5' },
-        { name: 'Type 6' },
-        { name: 'Type 7' },
-        { name: 'Type 8' },
-        { name: 'Type 9' },
-        { name: 'Type 10' },
-        { name: 'Type 11' },
-        { name: 'Type 12' },
-        { name: 'Type 13' },
-        { name: 'Type 14' },
-        { name: 'Type 15' },
-        { name: 'Type 16' },
-        { name: 'Type 17' },
-        { name: 'Type 18' },
-      ]
+      
+    const generationComponents = generations.map(generation => {
+        return <option value={generation}>{generation}</option>
+    })
+
+    const typeComponents = types.map(type => {
+        return <option value={type}>{type}</option>
+  })
+
 
     return(
         <div className="Header">
@@ -45,10 +22,18 @@ export default function Header(){
                 <img src="pokedex.svg" alt="" className="w-50"/>
                 <span className="font-pokemon text-5xl text-yellow-500">DEX</span>
             </div>
-            <div className="filters">
-                <br />
-                <Filter content={generations}></Filter>
-                <Filter content={types}></Filter>
+            <div className="filters flex">
+                <select name="generations" id="generations">
+                    {generationComponents}
+                </select>
+                <select name="types" id="types">
+                    {typeComponents}
+                </select>
+                <span>Number</span>
+                <span>Name</span>
+                <span>Weight</span>
+                <span>Size</span>
+                <input type="text" name="" id="" />
             </div>
         </div>
     )
