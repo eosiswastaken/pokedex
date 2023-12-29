fa1b0c378467f6b9779306d26e55a80b441acd62
TheLordGibril <gabidule03@gmail.com>
2023-12-29 21:14:33 +0100

    Switch between non shiny and shiny image in individual cards

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -3,12 +3,12 @@ import useAPI from "../context/Context";
 import Card from "./card";
 
 export default function PokemonList() {
-    const { pokemons, pokemonTypes , loading } = useAPI();
+    const { pokemons, pokemonTypes, loading } = useAPI();
     const pokemonComponents = pokemons.map(pk => {
         return <Card key={pk.id} pokemon={pk} pokemonTypes={pokemonTypes}></Card>
     })
 
-    
+
 
     return (
         <div className="flex flex-wrap gap-2 justify-center text-white text-xs">
