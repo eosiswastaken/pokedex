45b30834e7cc3d04d6348ecabb89593b38c8175e
TheLordGibril <gabidule03@gmail.com>
2023-12-29 18:12:11 +0100

    Evolution tree

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -21,33 +21,60 @@ export default function Card() {
                             <div className="text-center">{u.name.en}</div>
                             <div className="w-full h-[16px] flex justify-start">Gen. {u.generation}</div>
                         </div>
-                    </div>} modal={true}>
-                        <div className="w-[500px] h-[500px] rounded-md text-white text-xs bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1" >
-                            <div className="h-full w-full flex flex-col items-center justify-between bg-gray-800 back p-1">
-                                <div className="w-full h-[16px] flex justify-between">
-                                    <div>#{u.id}</div>
-                                    <div className="flex flex-row-reverse flex-wrap justify-between content-center gap-x-1">
-                                        <img className="h-[12px] rounded-full" src={pokemonTypes.find(type => type.id === u.types[0])?.image} alt="" />
-                                        <img className="h-[12px] rounded-full" src={pokemonTypes.find(type => type.id === u.types[1])?.image} alt="" />
+                    </div>} nested modal>
+                        {close => (
+                            <div className="w-[500px] h-[500px] rounded-md text-white text-xs bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1" >
+                                <div className="h-full w-full flex flex-col items-center justify-between bg-gray-800 back p-1">
+                                    <div className="w-full flex justify-between">
+                                        <div className="flex flex-wrap justify-between content-center gap-x-1">
+                                            <img className="h-[48px] rounded-full" src={pokemonTypes.find(type => type.id === u.types[0])?.image} alt="" />
+                                            <img className="h-[48px] rounded-full" src={pokemonTypes.find(type => type.id === u.types[1])?.image} alt="" />
+                                            <div className="text-base">#{u.id}</div>
+                                        </div>
+                                        <button className="close text-2xl " onClick={close}>
+                                            &times;
+                                        </button>
                                     </div>
+                                    <div className="w-full flex justify-center">
+                                        <div className="flex flex-col justify-center">
+                                            {Object.entries(u?.evolvedFrom).map(([id, level]) => (
+                                                <div className="flex flex-col items-center">
+                                                    <img className="h-[38px]" src={pokemons.find(pokemon => pokemon.id == id)?.image} />
+                                                    <div className="text-center">
+                                                        {level}
+                                                    </div>
+                                                </div>))}
+                                        </div>
+                                        <div>
+                                            <img className="h-[152px]" src={u.image} alt="" />
+                                            <div className="text-center text-base">{u.name.en}</div>
+                                        </div>
+                                        <div className="flex flex-col justify-center">
+                                            {Object.entries(u?.evolvesTo).map(([id, level]) => (
+                                                <div className="flex flex-col items-center">
+                                                    <img className="h-[38px]" src={pokemons.find(pokemon => pokemon.id == id)?.image} />
+                                                    <div className="text-center">
+                                                        {level}
+                                                    </div>
+                                                </div>))}
+                                        </div>
+                                    </div>
+                                    <div className="flex justify-between gap-10">
+                                        <div>Height : {u.height} m</div>
+                                        <div>Weight : {u.weight} kg</div>
+                                    </div>
+                                    <div className="flex gap-3">
+                                        <div>HP : {u.stats.hp}</div>
+                                        <div>ATK : {u.stats.atk}</div>
+                                        <div>DEF : {u.stats.def}</div>
+                                        <div>SPATK : {u.stats.spe_atk}</div>
+                                        <div>SPDEF : {u.stats.spe_def}</div>
+                                        <div>SPD : {u.stats.vit}</div>
+                                    </div>
+                                    <div className="w-full flex justify-start text-base">Gen. {u.generation}</div>
                                 </div>
-                                <div className="w-full flex justify-center"><img className="h-[76px]" src={u.image} alt="" /></div>
-                                <div className="text-center">{u.name.en}</div>
-                                <div className="flex justify-between gap-10">
-                                    <div>Height : {u.height} m</div>
-                                    <div>Weight : {u.weight} kg</div>
-                                </div>
-                                <div className="flex gap-3">
-                                    <div>HP : {u.stats.hp}</div>
-                                    <div>ATK : {u.stats.atk}</div>
-                                    <div>DEF : {u.stats.def}</div>
-                                    <div>SPATK : {u.stats.spe_atk}</div>
-                                    <div>SPDEF : {u.stats.spe_def}</div>
-                                    <div>SPD : {u.stats.vit}</div>
-                                </div>
-                                <div className="w-full h-[16px] flex justify-start">Gen. {u.generation}</div>
                             </div>
-                        </div>
+                        )}
                     </Popup>
                 </div>
             ))
